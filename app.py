4c74f78b094a85e8969ddb0086cf033b383ac389
thomas foucque <fouque.thomas@gmail.com>
2024-07-12 15:45:00 +0200

    Customize Streamlit app with sliders and map

--- app.py
+++ app.py
@@ -1,29 +1,58 @@
 import streamlit as st
 import requests
 import datetime
+import folium
+from streamlit_folium import folium_static
 
-'''
-# TaxiFareModel front
-'''
+# Style customization
+st.markdown(
+    """
+    <style>
+    .main {
+        background-color: #f5f5f5;
+    }
+    .stTextInput, .stNumberInput {
+        background-color: #ffffff;
+        border: 1px solid #cccccc;
+        border-radius: 4px;
+        padding: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Header and logo
+st.image("https://path/to/your/logo.png", width=100)
+st.title("TaxiFareModel Front")
 
 st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+## Enter the details of your ride:
 ''')
 
-# User inputs
-pickup_datetime = st.text_input("Pickup Date and Time", value=str(datetime.datetime.now()))
-pickup_longitude = st.number_input("Pickup Longitude", value=-73.985428)
-pickup_latitude = st.number_input("Pickup Latitude", value=40.748817)
-dropoff_longitude = st.number_input("Dropoff Longitude", value=-73.985428)
-dropoff_latitude = st.number_input("Dropoff Latitude", value=40.748817)
+# Date and time input
+pickup_date = st.date_input("Pickup Date", value=datetime.datetime.now().date())
+pickup_time = st.time_input("Pickup Time", value=datetime.datetime.now().time())
+pickup_datetime = datetime.datetime.combine(pickup_date, pickup_time)
+
+# Coordinate inputs using sliders
+pickup_longitude = st.slider("Pickup Longitude", min_value=-74.05, max_value=-73.75, value=-73.985428)
+pickup_latitude = st.slider("Pickup Latitude", min_value=40.63, max_value=40.85, value=40.748817)
+dropoff_longitude = st.slider("Dropoff Longitude", min_value=-74.05, max_value=-73.75, value=-73.985428)
+dropoff_latitude = st.slider("Dropoff Latitude", min_value=40.63, max_value=40.85, value=40.748817)
 passenger_count = st.number_input("Passenger Count", value=1, min_value=1, max_value=8)
+
+# Map
+st.markdown("### Pickup and Dropoff Locations")
+m = folium.Map(location=[40.748817, -73.985428], zoom_start=12)
+folium.Marker([pickup_latitude, pickup_longitude], tooltip='Pickup').add_to(m)
+folium.Marker([dropoff_latitude, dropoff_longitude], tooltip='Dropoff').add_to(m)
+folium_static(m)
 
 # Call the API
 url = 'https://taxifare.lewagon.ai/predict'
 params = {
-    "pickup_datetime": pickup_datetime,
+    "pickup_datetime": pickup_datetime.strftime("%Y-%m-%d %H:%M:%S"),
     "pickup_longitude": pickup_longitude,
     "pickup_latitude": pickup_latitude,
     "dropoff_longitude": dropoff_longitude,
